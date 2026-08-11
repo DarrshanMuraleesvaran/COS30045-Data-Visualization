4e39b63a7c5b3c0d59bb43d9a6d4accc25ee5ce1
DarrshanMuraleesvaran <105965528@student.swin.edu.au>
2026-08-12 03:09:05 +0800

    Add interactive FAQ accordion using JavaScript

---
+++ Exercise 0.2/energy-webpage-v1/js/scripts.js
@@ -0,0 +1,13 @@
+const faqQuestions = document.querySelectorAll(".faq-question");
+
+faqQuestions.forEach(function (question) {
+    question.addEventListener("click", function () {
+        const answer = question.nextElementSibling;
+
+        if (answer.style.display === "block") {
+            answer.style.display = "none";
+        } else {
+            answer.style.display = "block";
+        }
+    });
+});
