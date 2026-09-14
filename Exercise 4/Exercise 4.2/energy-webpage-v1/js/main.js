0e8c39e5c0f09492bae61f72cfc181b3dac11dfc
DarrshanMuraleesvaran <105965528@student.swin.edu.au>
2026-09-15 01:45:37 +0800

    Complete D3 manipulation for Exercise 4.2
    Used D3 to style an existing heading, append a new paragraph to a div element, and add a visible rectangle to the SVG area.

--- Exercise 4/Exercise 4.2/energy-webpage-v1/js/main.js
+++ Exercise 4/Exercise 4.2/energy-webpage-v1/js/main.js
@@ -1,2 +1,19 @@
+// Step 2: Change the style of an HTML element
 d3.select("h1")
   .style("color", "green");
+
+
+// Step 3: Append a paragraph to the D3 demo section
+d3.select(".d3-demo")
+  .append("p")
+  .text("Purchasing a low energy consumption TV will help with your energy bills!");
+
+
+// Step 4: Append a rectangle to the SVG
+d3.select("#d3-demo-svg")
+  .append("rect")
+  .attr("x", 50)
+  .attr("y", 50)
+  .attr("width", 100)
+  .attr("height", 30)
+  .style("fill", "green");
